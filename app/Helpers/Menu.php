3a31d6e0ea07913506ac349ecbc72ab38c8ba636
Ashfaq H Ahmed <ashfaqshuvo14@gmail.com>
2020-02-23 16:18:47 +0600

    Admin Lte integrated and activated.

---
+++ app/Helpers/Menu.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Support\Facades\Route;
+
+class Menu
+{
+
+    /*
+        |--------------------------------------------------------------------------
+        | Detect Active Route
+        |--------------------------------------------------------------------------
+        |
+        | Compare given route with current route and return output if they match.
+        | Very useful for navigation, marking if the link is active.
+        |
+        */
+    public static function isActiveRoute($route, $output = 'active')
+    {
+        if (Route::currentRouteName() == $route) {
+            return $output;
+        }
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Detect Active Routes
+    |--------------------------------------------------------------------------
+    |
+    | Compare given routes with current route and return output if they match.
+    | Very useful for navigation, marking if the link is active.
+    |
+    */
+    public static function areActiveRoutes(array $routes, $output = 'active')
+    {
+        foreach ($routes as $route) {
+            if (Route::currentRouteName() == $route) {
+                return $output;
+            }
+        }
+    }
+}
---
+++ resources/views/admin/includes/footer.blade.php
@@ -0,0 +1,14 @@
+
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 0.0.1
+    </div>
+    <strong>Copyright &copy; {{ date('Y')}} <a href="http://ashfaqhahmed.com">Ashfaq H Ahmed</a>.</strong> All rights
+    reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
